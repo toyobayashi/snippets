774f775e04a452ea9aa827df3b6765ba09df87d1
toyobayashi <lifenglin314@outlook.com>
2020-07-16 11:31:47 +0800

    copy

--- copy/copy.js
+++ copy/copy.js
@@ -6,8 +6,10 @@
 function copy (str) {
   var i = document.createElement('input');
   i.value = str;
+  i.setAttribute('readonly', 'readonly');
   document.body.appendChild(i);
   i.select();
+  i.setSelectionRange(0, i.value.length);
   var r;
   try {
     r = document.execCommand('copy');
